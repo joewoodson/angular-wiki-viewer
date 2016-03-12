71dfa5cb2e40ac1574c7aa0825c040fcafafb079
Joe Woodson <contact@joewoodson.de>
2016-03-12 17:45:14 +0100

    return results list instead of single entry

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,35 @@
  * Controller of the wikiReaderApp
  */
 angular.module('wikiReaderApp')
+  // .factory('wikiService', function($http) {
+
+  // 	var wikiService = {
+  // 		get: function(wikiQuery) {
+  // 			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&callback=JSON_CALLBACK&titles=' + wikiQuery.name)
+  // 		}
+  // 	};
+
+  // 	return wikiService;
+  // })
+  // .controller('MainCtrl', function ($scope, wikiService) {
+
+  // 	$scope.wikiQuery = '';
+
+  // 	$scope.$watch('wikiQuery', function() {
+  // 		wikiService.get({name: $scope.wikiQuery}).then(function(data) {
+
+	 //        var pagesId = Object.keys(data.data.query.pages);
+
+	 //        $scope.wikiExtract = data.data.query.pages[pagesId].extract;
+	 //        $scope.wikiTitle = data.data.query.pages[pagesId].title;
+	 //    });
+  // 	});
+
   .factory('wikiService', function($http) {
 
   	var wikiService = {
   		get: function(wikiQuery) {
-  			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&callback=JSON_CALLBACK&titles=' + wikiQuery.name)
+  			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&callback=JSON_CALLBACK&srsearch=' + wikiQuery.name)
   		}
   	};
 
@@ -21,16 +45,20 @@ angular.module('wikiReaderApp')
   .controller('MainCtrl', function ($scope, wikiService) {
 
   	$scope.wikiQuery = '';
+  	$scope.wikiResults = [];
 
-  	$scope.$watch('wikiQuery', function() {
-  		wikiService.get({name: $scope.wikiQuery}).then(function(data) {
+  	$scope.$watch('wikiQuery', function() {  	
+		wikiService.get({name: $scope.wikiQuery}).then(function(data) {
 
-	        var pagesId = Object.keys(data.data.query.pages);
+			if ($scope.wikiQuery) {
+			    for (var result of data.data.query.search) {
+						$scope.wikiResults.push(result);
+				}
+			}
 
-	        $scope.wikiExtract = data.data.query.pages[pagesId].extract;
-	        $scope.wikiTitle = data.data.query.pages[pagesId].title;
-	    });
-  	});
+		});
 
-    
+		$scope.wikiResults = [];
+	});
+
   });
