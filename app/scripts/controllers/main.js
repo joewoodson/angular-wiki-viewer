c290d458b53b560e253ca27978c23443924e550d
Joe Woodson <contact@joewoodson.de>
2016-03-12 19:32:28 +0100

    add article snippets and sliding panels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,30 +8,6 @@
  * Controller of the wikiReaderApp
  */
 angular.module('wikiReaderApp')
-  // .factory('wikiService', function($http) {
-
-  // 	var wikiService = {
-  // 		get: function(wikiQuery) {
-  // 			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&callback=JSON_CALLBACK&titles=' + wikiQuery.name)
-  // 		}
-  // 	};
-
-  // 	return wikiService;
-  // })
-  // .controller('MainCtrl', function ($scope, wikiService) {
-
-  // 	$scope.wikiQuery = '';
-
-  // 	$scope.$watch('wikiQuery', function() {
-  // 		wikiService.get({name: $scope.wikiQuery}).then(function(data) {
-
-	 //        var pagesId = Object.keys(data.data.query.pages);
-
-	 //        $scope.wikiExtract = data.data.query.pages[pagesId].extract;
-	 //        $scope.wikiTitle = data.data.query.pages[pagesId].title;
-	 //    });
-  // 	});
-
   .factory('wikiService', function($http) {
 
   	var wikiService = {
