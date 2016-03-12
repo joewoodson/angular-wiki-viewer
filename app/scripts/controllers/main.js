db9b1c9cdac96062261d218a097519fa56c435f0
Joe Woodson <contact@joewoodson.de>
2016-03-12 16:37:28 +0100

    add instant search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,17 +12,25 @@ angular.module('wikiReaderApp')
 
   	var wikiService = {
   		get: function(wikiQuery) {
-  			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&callback=JSON_CALLBACK&titles=' + wikiQuery.name.toLowerCase())
+  			return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&callback=JSON_CALLBACK&titles=' + wikiQuery.name)
   		}
   	};
 
   	return wikiService;
   })
   .controller('MainCtrl', function ($scope, wikiService) {
-    wikiService.get({name: 'Germany'}).then(function(data) {
-        var pagesId = Object.keys(data.data.query.pages);
 
-        $scope.wikiExtract = data.data.query.pages[pagesId].extract;
-        console.log($scope.wikiExtract);
-    });
+  	$scope.wikiQuery = '';
+
+  	$scope.$watch('wikiQuery', function() {
+  		wikiService.get({name: $scope.wikiQuery}).then(function(data) {
+
+	        var pagesId = Object.keys(data.data.query.pages);
+
+	        $scope.wikiExtract = data.data.query.pages[pagesId].extract;
+	        $scope.wikiTitle = data.data.query.pages[pagesId].title;
+	    });
+  	});
+
+    
   });
